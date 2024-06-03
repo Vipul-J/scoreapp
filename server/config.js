a8dedf561a75cf1f94e4c0af99e0f9d45e72a3b5
Vipul-J <jujarvipul21@gmail.com>
2024-06-04 02:58:52 +0530

    initial commit

--- server/config.js
+++ server/config.js
@@ -1,10 +1,10 @@
-// config.js
+require('dotenv').config();
+
 module.exports = {
-    mysql: {
-      host: 'localhost',
-      user: 'root',
-      password: 'new_password',
-      database: 'scoreapp'
-    }
-  };
-  
+  mysql: {
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
+  }
+};
